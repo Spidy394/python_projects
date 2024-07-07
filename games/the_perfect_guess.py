98ed459a6e03f8349da33b8e4b4117dd04fc0522
Spidy394 <mondalshubhodeep30924@gmail.com>
2024-07-07 21:48:53 +0530

    added the perfect game

--- games/the_perfect_guess.py
+++ games/the_perfect_guess.py
@@ -1,23 +1,52 @@
-import pyttsx3
 import random
+import pyttsx3
 
-def main(): 
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)
+engine = pyttsx3.init()
+engine.setProperty('rate', 250)
+greetings = "Welcome to the Perfect guess game"
+higher = "Higher number please"
+lower = "Lower number please"
+repeat_message = "Do you want to play again"
+bye_message = "Thanks for playing"
+
+def ask_to_repeat():
+    engine.say(repeat_message)
+    engine.runAndWait()
+    repeat = input(f"{repeat_message}? (y/n) ")
+    if repeat.lower() == 'y':
+        main()
+    else:
+        print(f"{bye_message}!")
+        engine.say(bye_message)
+        engine.runAndWait()
+
+def main():
     a = random.randint(1, 100)
     guess_count = 1
-    greetings = "Welcome to the Perfect guess game"
-    print(f"|| {greetings} ||")
+    
+    print(f"\n|| {greetings} ||")
     engine.say(greetings)
+    engine.runAndWait()
+
     while True: 
         n = int(input("Make your guess: "))
         if (a > n):
-            print("Higher number please: ")
+            engine.say(higher)
+            engine.runAndWait()
+            print(f"{higher}: ")
             guess_count += 1 
-        elif (a < n) :
-            print("Lower number please")
+        elif (a < n):
+            engine.say(lower)
+            engine.runAndWait()
+            print(f"{lower}: ")
             guess_count += 1
         else:
+            win_message = f"Bingo! You have guessed it right in {guess_count} tries!"
+            print(f"{win_message}")
+            engine.say(win_message)
+            engine.runAndWait()
+            ask_to_repeat()
             break
-
-    print(f"Bingo! You have guessed it right in {guess_count} tries!")
+   
+if __name__ == "__main__":
+    main()
